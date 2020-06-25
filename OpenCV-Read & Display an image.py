ea7b1e9d21fcd5eb8795055d76a8befd396aca7f
Prubhtej Singh <47414674+PRUBHTEJ@users.noreply.github.com>
2020-06-25 15:12:06 +0530

    Update and rename ps.py to OpenCV-Read & Display an image.py

--- OpenCV-Read & Display an image.py
+++ OpenCV-Read & Display an image.py
@@ -1,3 +1,4 @@
+# This program uses the OpenCV library to read & display the image on the screen.
 import cv2
 
 img = cv2.imread('profile.png')
@@ -7,7 +8,6 @@ gray = cv2.imread('profile.png',cv2.IMREAD_GRAYSCALE)
 cv2.imshow('pf-pic',img)
 
 cv2.imshow('pf-pic-gray',gray)
-
 
 cv2.waitKey(0)
 
